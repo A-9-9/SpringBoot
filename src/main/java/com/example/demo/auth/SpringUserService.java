aa434660e122811f913e0b1fb284101448b8ec45
Ken <10846013@ntub.edu.tw>
2021-11-16 16:38:25 +0800

    implements UserDetail.

--- src/main/java/com/example/demo/auth/SpringUserService.java
+++ src/main/java/com/example/demo/auth/SpringUserService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.app_user.AppUser;
+import com.example.demo.entity.app_user.SpringUser;
 import com.example.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +12,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SpringUserService implements UserDetailsService {
@@ -20,7 +24,12 @@ public class SpringUserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try {
             AppUser appUser = appUserService.getUserByEmail(username);
-            return new User(appUser.getEmailAddress(), appUser.getPassword(), Collections.emptyList());
+            
+            return new SpringUser(appUser);
+//            List<SimpleGrantedAuthority> authorities = appUser.getAuthorityList()
+//                    .stream().map(a -> new SimpleGrantedAuthority(a.name()))
+//                    .collect(Collectors.toList());
+//            return new User(appUser.getEmailAddress(), appUser.getPassword(), authorities);
         } catch (NotFoundException e){
             throw new UsernameNotFoundException("Username is wrong.");
         }
